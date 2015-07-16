fe256ee2b5efec203b2026864e88cae1f7041137
Hemal Patel <hemal@xfinion.com>
2015-07-16 00:24:12 -0400

    Initial Authentication - login, create user completed.

---
+++ FareDealApi/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using FareDeal.Service;
+
+namespace FareDealApi.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public string Get()
+        {
+            var userName = this.RequestContext.Principal.Identity.Name;
+            //return String.Format("Hello, {0}.", userName);
+
+            FareDealDbContext dbContext = new FareDealDbContext();
+            List<Restaurent> rs = dbContext.Restaurents.ToList();
+            return String.Format("Total  {0} records found", rs.Count());
+        }
+    }
+}
---
+++ FareDealApi/Models/RestaurentModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LocalAccountsApp.Models
+{
+    // Models returned by AccountController actions.
+
+    public class Restaurent
+    {
+        public int RestaurentId { get; set; }
+        public string RestaurentName { get; set; }
+
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+
+        public string State { get; set; }
+        public string Zip { get; set; }
+    } 
+}

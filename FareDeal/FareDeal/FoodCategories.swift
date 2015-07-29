a96b8531154ebe809128d5697333c20b1d9dc114
g67277 <nshuqair@fullsail.edu>
2015-07-29 09:28:22 -0400

    Working on business registration form

---
+++ FareDeal/FareDeal/FoodCategories.swift
@@ -0,0 +1,13 @@
+//
+//  FoodCategories.swift
+//  FareDeal
+//
+//  Created by Nazir Shuqair on 7/28/15.
+//  Copyright (c) 2015 SNASTek. All rights reserved.
+//
+
+import Cocoa
+
+class FoodCategories: NSObject {
+
+}
---
+++ FareDeal/FareDeal/RegisterRestaurantVC2.swift
@@ -0,0 +1,13 @@
+//
+//  RegisterRestaurantVC2.swift
+//  FareDeal
+//
+//  Created by Nazir Shuqair on 7/28/15.
+//  Copyright (c) 2015 SNASTek. All rights reserved.
+//
+
+import Cocoa
+
+class RegisterRestaurantVC2: UIViewController {
+
+}

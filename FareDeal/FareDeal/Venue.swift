a46fc9b13ebe16edd54629e43bbb9af266aba853
Angela Smith <angessmith@gmail.com>
2015-08-02 23:25:24 -0500

    Users home view gathers location and pulls foursquare data instead of from plist. Data persists with realm. Favorites view loads saved venues, and detail view loads venue details. *See note on top of home view for location enabling without plist push.

---
+++ FareDeal/FareDeal/Venue.swift
@@ -0,0 +1,18 @@
+//
+//  Venue.swift
+//  FareDeal
+//
+//  Created by Angela Smith on 8/2/15.
+//  Copyright (c) 2015 SNASTek. All rights reserved.
+//
+
+import RealmSwift
+
+class Venue: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}

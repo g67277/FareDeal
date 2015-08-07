f23301024d4a5a3089622c7369f5ca274df8f28b
g67277 <nshuqair@fullsail.edu>
2015-08-07 02:10:49 -0400

    profile page 90% done

--- FareDeal/FareDeal/ProfileVC.swift
+++ FareDeal/FareDeal/ProfileVC.swift
@@ -7,16 +7,238 @@
 //
 
 import UIKit
+import MobileCoreServices
+import ActionSheetPicker_3_0
 
-class ProfileVC: UIViewController {
+class ProfileVC: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    
+    @IBOutlet weak var RestaurantTitleLabel: UILabel!
+    @IBOutlet weak var imgView: UIImageView!
+    var newMedia: Bool?
+    var validImage = false
+    @IBOutlet weak var contactField: UITextField!
+    //Category
+    @IBOutlet weak var catButton: UIButton!
+    var categories:FoodCategories = FoodCategories()
+    var categoryArray = []
+    // Price Tier
+    @IBOutlet weak var priceControls: UISegmentedControl!
+    var selectedPrice = 0
+    var validPrice = false
+    //Hours
+    @IBOutlet weak var weekdayO: UIButton!
+    @IBOutlet weak var weekdayC: UIButton!
+    @IBOutlet weak var weekendO: UIButton!
+    @IBOutlet weak var weekendC: UIButton!
+    let prefs: NSUserDefaults = NSUserDefaults.standardUserDefaults()
 
     
     
     override func viewDidLoad() {
-        super .viewDidLoad()
+        super.viewDidLoad()
         
+        var tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "DismissKeyboard")
+        view.addGestureRecognizer(tap)
+        
+        categoryArray = categories.loadCategories()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        imgView.layer.masksToBounds = false
+        imgView.layer.borderColor = UIColor.blackColor().CGColor
+        imgView.layer.cornerRadius = imgView.frame.height/2
+        imgView.clipsToBounds = true
+    }
+    
+    func DismissKeyboard(){
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func onClick(_sender:UIButton){
+        
+        if _sender.tag == 0{
+            
+            //save here
+            
+        }else if _sender.tag == 1{
+            self.startImageAction()
+        }else if _sender.tag == 2{
+            // Log out here
+            self.signOff()
+        }
+        
+    }
+    
+    @IBAction func pickerSelected(sender: AnyObject) {
+        
+        if sender.tag == 4 {
+            ActionSheetStringPicker.showPickerWithTitle("Category", rows: categoryArray as [AnyObject], initialSelection: 1, doneBlock: {
+                picker, value, index in
+                
+                self.catButton.setTitle("\(index)", forState: UIControlState.Normal)
+                
+                println("value = \(value)")
+                println("index = \(index)")
+                println("picker = \(picker)")
+                return
+                }, cancelBlock: { ActionStringCancelBlock in return }, origin: sender)
+            
+        }else if sender.tag < 4{
+            
+            ActionSheetStringPicker.showPickerWithTitle("Time", rows: ["12am", "1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"] as [AnyObject], initialSelection: 1, doneBlock: {
+                picker, value, index in
+                
+                if sender.tag == 0 {
+                    self.weekdayO.setTitle("\(index)", forState: UIControlState.Normal)
+                }else if sender.tag == 1{
+                    self.weekdayC.setTitle("\(index)", forState: UIControlState.Normal)
+                }else if sender.tag == 2{
+                    self.weekendO.setTitle("\(index)", forState: UIControlState.Normal)
+                }else if sender.tag == 3{
+                    self.weekendC.setTitle("\(index)", forState: UIControlState.Normal)
+                }
+                
+                println("value = \(value)")
+                println("index = \(index)")
+                println("picker = \(picker)")
+                return
+                }, cancelBlock: { ActionStringCancelBlock in return }, origin: sender)
+            
+        }
         
         
     }
+    
+    
+    @IBAction func priceControl(sender: AnyObject) {
+        
+        switch sender.selectedSegmentIndex{
+        case 0:
+            selectedPrice = 0
+        case 1:
+            selectedPrice = 1
+        case 2:
+            selectedPrice = 2
+        case 3:
+            selectedPrice = 3
+        default:
+            break;
+        }
+    }
+    
+    
+    func signOff(){
+        prefs.setObject(nil, forKey: "TOKEN")
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    
+    // Camera methods
+    
+    func startImageAction(){
+        // Take picture here
+        
+        //Create the AlertController
+        let actionSheetController: UIAlertController = UIAlertController(title: "Upload a picture", message: "It can be of your restaurant or a of a dish off your menu", preferredStyle: .ActionSheet)
+        
+        //Create and add the Cancel action
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in
+            //Just dismiss the action sheet
+        }
+        actionSheetController.addAction(cancelAction)
+        //Create and add first option action
+        let takePictureAction: UIAlertAction = UIAlertAction(title: "Take Picture", style: .Default) { action -> Void in
+            //Code for launching the camera goes here
+            if UIImagePickerController.isSourceTypeAvailable(
+                UIImagePickerControllerSourceType.Camera) {
+                    
+                    let imagePicker = UIImagePickerController()
+                    
+                    imagePicker.delegate = self
+                    imagePicker.sourceType =
+                        UIImagePickerControllerSourceType.Camera
+                    imagePicker.mediaTypes = [kUTTypeImage as NSString]
+                    imagePicker.allowsEditing = false
+                    
+                    self.presentViewController(imagePicker, animated: true,
+                        completion: nil)
+                    self.newMedia = true
+            }
+            
+        }
+        actionSheetController.addAction(takePictureAction)
+        //Create and add a second option action
+        let choosePictureAction: UIAlertAction = UIAlertAction(title: "Choose From Camera Roll", style: .Default) { action -> Void in
+            //Code for picking from camera roll goes here
+            if UIImagePickerController.isSourceTypeAvailable(
+                UIImagePickerControllerSourceType.SavedPhotosAlbum) {
+                    let imagePicker = UIImagePickerController()
+                    
+                    imagePicker.delegate = self
+                    imagePicker.sourceType =
+                        UIImagePickerControllerSourceType.PhotoLibrary
+                    imagePicker.mediaTypes = [kUTTypeImage as NSString]
+                    imagePicker.allowsEditing = false
+                    self.presentViewController(imagePicker, animated: true,
+                        completion: nil)
+                    self.newMedia = false
+            }
+        }
+        
+        actionSheetController.addAction(choosePictureAction)
+        
+        //We need to provide a popover sourceView when using it on iPad
+        //actionSheetController.popoverPresentationController?.sourceView = sender as! UIView;
+        
+        //Present the AlertController
+        self.presentViewController(actionSheetController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        
+        let mediaType = info[UIImagePickerControllerMediaType] as! NSString
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        if mediaType.isEqualToString(kUTTypeImage as! String) {
+            let image = info[UIImagePickerControllerOriginalImage]
+                as! UIImage
+            
+            imgView.image = image
+            validImage = true
+            
+            if (newMedia == true) {
+                UIImageWriteToSavedPhotosAlbum(image, self,
+                    "image:didFinishSavingWithError:contextInfo:", nil)
+            } else if mediaType.isEqualToString(kUTTypeMovie as! String) {
+                // Code to support video here
+            }
+            
+        }
+    }
+    
+    func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo:UnsafePointer<Void>) {
+        
+        if error != nil {
+            let alert = UIAlertController(title: "Save Failed",
+                message: "Failed to save image",
+                preferredStyle: UIAlertControllerStyle.Alert)
+            
+            let cancelAction = UIAlertAction(title: "OK",
+                style: .Cancel, handler: nil)
+            
+            alert.addAction(cancelAction)
+            self.presentViewController(alert, animated: true,
+                completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
     
 }

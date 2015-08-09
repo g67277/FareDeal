77dee5a020a9649230338173fed405398a8e7a75
g67277 <nshuqair@fullsail.edu>
2015-08-08 20:45:33 -0400

    Changes to storyboard

--- FareDeal/FareDeal/ForgotPasswordUserVC.swift
+++ FareDeal/FareDeal/ForgotPasswordUserVC.swift
@@ -59,23 +59,7 @@ class ForgotPasswordUserVC: UIViewController {
     
     override func viewWillAppear(animated: Bool) {
         // Hide the navigation bar to display the full location image
-        let navBar:UINavigationBar! =  self.navigationController?.navigationBar
-        if navBar != nil{
-            self.navigationController!.navigationBar.hidden = true
-            navBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
-            navBar.shadowImage = UIImage()
-            navBar.backgroundColor = UIColor.clearColor()
-        }
-    }
-    
-    
-    override func viewWillDisappear(animated: Bool) {
-        // restore the navigation bar to origional
-        let navBar:UINavigationBar! =  self.navigationController?.navigationBar
-        if navBar != nil{
-            navBar.setBackgroundImage(nil, forBarMetrics: UIBarMetrics.Default)
-            navBar.shadowImage = nil
-        }
+        navigationController?.navigationBarHidden = true
     }
 
     
